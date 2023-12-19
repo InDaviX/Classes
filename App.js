635eabdf5cc0b5dbccddbe04df152144c76db3a3
InDaviX <dszewczyk235@gmail.com>
2023-12-19 15:53:08 +0100

    Simple state changing

--- App.js
+++ App.js
@@ -4,11 +4,17 @@ import {SafeAreaView, Text} from 'react-native';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
   render() {
     return (
       <SafeAreaView>
-        <Text>Hello World!</Text>
+        <Text
+          onPress={() => {
+            this.setState({name: 'Dawid'});
+          }}>
+          Hello {this.state?.name}!
+        </Text>
       </SafeAreaView>
     );
   }
